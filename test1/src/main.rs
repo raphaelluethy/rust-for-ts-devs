383a68599a2bef9be608b4a8baca5d930eecb50f
Raphael Lüthy <raphael@famluethy.ch>
2023-06-23 23:17:29 +0200

    init first playground project to test settings

---
+++ test1/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("Hello, world!");
+    let x = 5;
+    x += 5;
+    println!("The value of x is: {}", x);
+    let mut y = 10;
+    y += 10;
+    println!("The value of x is: {}", y);
+}
